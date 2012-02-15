8c4bf103efd107fcf122250d606a1d4f897a3ca3
rodrigo koblitz <rodrigokoblitz@gmail.com>
2012-02-15 02:37:54 -0200

    variavel heroku 14

--- tblmet/settings.py
+++ tblmet/settings.py
@@ -26,7 +26,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'biomet',                      # Or path to database file if using sqlite3.
             'USER': 'postgres',                      # Not used with sqlite3.
-            #'PASSWORD': 'bd',#os.environ.get('PASSWORD',  ''), 
+            
             'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
      #       'OPTIONS': {
