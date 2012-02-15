1dcf706c7a63f4d0ba1a354611d7be31b933ae2c
rodrigo koblitz <rodrigokoblitz@gmail.com>
2012-02-15 00:38:28 -0200

    variavel de ambiente heroku1

--- tblmet/settings.py
+++ tblmet/settings.py
@@ -19,7 +19,7 @@ if 'True'==os.environ.get('PASSWORD', 'False'):
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'biomet',                      # Or path to database file if using sqlite3.
             'USER': 'postgres',                      # Not used with sqlite3.
-      
+            'PASSWORD': os.environ.get('PASSWORD',  ''), 
             'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
      #       'OPTIONS': {
