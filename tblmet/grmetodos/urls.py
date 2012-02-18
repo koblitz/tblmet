4460f89d7e4cb2e6ae5563213a834ee64cf3c0b5
rodrigo koblitz <rodrigokoblitz@gmail.com>
2012-02-17 23:53:45 -0200

    acerta admin1

--- tblmet/grmetodos/urls.py
+++ tblmet/grmetodos/urls.py
@@ -26,5 +26,5 @@ urlpatterns = patterns('grmetodos.views',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
