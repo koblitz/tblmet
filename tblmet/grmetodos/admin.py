d2b509c8e42619fd16cef65d3d85cb50e1cdf3dd
rodrigo koblitz <rodrigokoblitz@gmail.com>
2012-02-17 23:48:40 -0200

    coloca admin

---
+++ tblmet/grmetodos/admin.py
@@ -0,0 +1,6 @@
+#-*- coding: utf-8 -*-
+
+from grmetodos.models import *
+from django.contrib import admin
+
+admin.site.register([ TblUnidades, TblAtribs, TblLocais])
--- tblmet/settings.py
+++ tblmet/settings.py
@@ -136,7 +136,7 @@ INSTALLED_APPS = (
     'consultas', 
     'atributos', 
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
